3b4b8e80a6f8320d43bd4f1e20a1bf7c2f5e162a
Gleb <st090625@gmail.com>
2023-07-16 17:34:40 +0300

    chore: added change to store

--- src/store.ts
+++ src/store.ts
@@ -6,12 +6,7 @@ export interface ApplicationState {
   done: Record<number, boolean>
 }
 
-const items = [
-  "Позавтракать",
-  "Сходить в магазин",
-  "Покормить кота",
-  "Забыть поспать",
-]
+const items = ["Позавтракать", "Сходить в магазин", "Покормить кота"]
 
 const DEFAULT_STATE = { items, done: {}, text: "" }
 
